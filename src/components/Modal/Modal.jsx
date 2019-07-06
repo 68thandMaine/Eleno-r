9f14678b394caec6d8cf83cd272292e9c852e3fb
Chris Rudnicky <chrisrudnicky@gmail.com>
2019-07-06 00:05:17 -0700

    add: restyle the modal body

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -5,15 +5,14 @@ import './Modal.css';
 
 const modal = (props) => {
   return (
-    <div className='modal-wrapper'>
-      <div className='modal-header'>
-        <h3>Modal Header</h3>
-      </div>
-      <div className='modal-body'>
-        <div>{props.children}</div>
-      </div>
-      <div className = 'modal-footer'>
-        <button className='btn-cancel' onClick={props.close}>Close</button>
+    <div className='modal-mask'>
+      <div className='modal-wrapper'>
+        <div className='modal-container'>
+          <div className='modal-body'>
+            <div>{props.children}</div>
+            <button className='btn-cancel' onClick={props.close}>Close</button>
+          </div>
+        </div>
       </div>
     </div>
   );
@@ -21,6 +20,6 @@ const modal = (props) => {
 
 modal.propTypes = {
   close: PropTypes.func.isRequired
-}
+};
 
 export default modal;
